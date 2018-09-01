8e54a2fb2ef5e9cef337cb5ff65cea6c5ba73f10
Dan Brimhall <dpbrimhall@hotmail.com>
2018-09-01 00:16:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,15 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) 
-  {
+  for (let i = 0; i < 25; i++) {
+    if (i === 0) {
       array.push(`I am ${i} strange loops.`)
+    }
+    else if (i === 1) {
+      array.push('I am 1 strange loops.')
      }
-      
+    else array.push(`I am ${i} strange loops.`)  
   }
+  return array
+}
 
   
   function whileLoop(n) {
